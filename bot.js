084f4b2d4f90462447a6dedad460bd7c03cd638d
Stefan Resendes <stefan.resendes@gmail.com>
2018-11-26 15:13:54 -0500

    test push

--- bot.js
+++ bot.js
@@ -14,15 +14,15 @@ client.on("ready", () => {
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-client.on("guildCreate", guild => {
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+// client.on("guildCreate", guild => {
+//     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+//     client.user.setActivity(`Serving ${client.guilds.size} servers`);
+// });
 
-client.on("guildDelete", guild => {
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+// client.on("guildDelete", guild => {
+//     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+//     client.user.setActivity(`Serving ${client.guilds.size} servers`);
+// });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel;
@@ -32,22 +32,24 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     var oldUserName = oldMember.user.username;
 
     if(oldUserChannel === undefined && newUserChannel !== undefined) {
-	    // User Joins a voice channel
+	// User Joins a voice channel
         //play introLink
         let user = returnUser(newUserName);
         newMember.voiceChannel.join()
             .then(connection => {
                 playAudioFromYoutube(connection, user.introLink);
-            });
+            })
+	.catch(console.log);
+	
 
-	    //client.channels.get(server).send(newUserName + ' joined');
+	//client.channels.get(server).send(newUserName + ' joined');
 
-	    //newMember.voiceChannel.join();
-	    // .then(connection => {
-	    // 	console.log('Connected');
-	    // 	const dispatcher = connection.playFile(musicFile);
-	    // })
-	    // .catch(console.log);
+	//newMember.voiceChannel.join();
+	// .then(connection => {
+	// 	console.log('Connected');
+	// 	const dispatcher = connection.playFile(musicFile);
+	// })
+	// .catch(console.log);
 
     } else if(newUserChannel === undefined){
 	// User leaves a voice channel
@@ -59,64 +61,44 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 
 client.on("message", async message => {
     if(message.author.bot)
-	    return;
+	return;
     
     if(message.content.indexOf(config.prefix) !== 0)
-	    return;
+	return;
     
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     if(command === "play") {
-	    console.log(message.member.voiceChannel);
-	    const link = args.join(" ");
-	    console.log(link);
-	    if (link === "demo") {
-	        message.member.voiceChannel.join()
-		    .then(connection => {
-		        const dispatcher = connection.playFile(musicFile);
-		    })
-		    .catch(console.log);
-	    } else if (link !== "") {
-	        //YouTube
+	console.log(message.member.voiceChannel);
+	const link = args.join(" ");
+	console.log(link);
+	if (link === "demo") {
+	    message.member.voiceChannel.join()
+		.then(connection => {
+		    const dispatcher = connection.playFile(musicFile);
+		})
+		.catch(console.log);
+	} else if (link !== "") {
+	    //YouTube
 	    
-	        console.log(link);
-	        message.member.voiceChannel.join()
-		    .then(connection => {
-	     	   // const stream = ytdl(link, {filter: 'audioonly'});
-		        //const streamOptions = {seek: 0, volume: 1};
-		        //const dispatcher = connection.playStream(stream, streamOptions);
-                playAudioFromYoutube(connection, link);
+	    console.log(link);
+	    message.member.voiceChannel.join()
+		.then(connection => {
+                    playAudioFromYoutube(connection, link);
 	     	})
 	     	.catch(console.log);
 	    
-	    }
+	}
     }
     
     if(command === "stop") {
-	    message.member.voiceChannel.leave()
+	message.member.voiceChannel.leave()
     }
     
     if(command === "ping") {
-	    const m = await message.channel.send("Ping?");
-	    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-    
-    if(command === "say") {
-	    const sayMessage = args.join(" ");
-	    message.delete().catch(O_o=>{}); 
-	    message.channel.send(sayMessage);
-    }
-    
-    if(command === "purge") {
-	    const deleteCount = parseInt(args[0], 10);
-	
-	if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-	    return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
-	const fetched = await message.channel.fetchMessages({limit: deleteCount});
-	message.channel.bulkDelete(fetched)
-	    .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	const m = await message.channel.send("Ping?");
+	m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
 });
 
