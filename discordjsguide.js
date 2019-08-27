09962bdd8bdf703b1de568af7dac2975c89842f4
tweis34 <tylerweis34@gmail.com>
2019-08-26 23:16:17 -0500

    added in stop functionality, commands are now in a switch

--- discordjsguide.js
+++ discordjsguide.js
@@ -3,28 +3,48 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
+const queue = new Map();
 
 client.on("ready", () => {
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    console.log('Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.');
+    client.user.setActivity('Serving ${client.guilds.size} servers');
 });
 
 client.on('message', message => {
-    if (message.content === '!play') {
-        if (message.channel.type !== 'text') return;
-        const voiceChannel = message.member.voice.channel;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-        if (!voiceChannel) {
-            return message.reply('please join a voice channel first!');
-        }
-
-        voiceChannel.join().then(connection => {
-            const stream = ytdl('https://www.youtube.com/watch?v=Qkuu0Lwb5EM', { filter: 'audioonly' });
-            const dispatcher = connection.play(stream);
-
-            dispatcher.on('end', () => voiceChannel.leave());
-        });
+    if (message.channel.type !== 'text') {
+        return;
     }
+
+    const link = args.join(" ");
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+        return message.reply('please join a voice channel first!');
+    }
+
+    switch (command) {
+        case 'play':
+            //youtube
+            voiceChannel.join().then(connection => {
+                const stream = ytdl(link, { filter: 'audioonly', quality: 'highestaudio', volume: '.75' });
+                const dispatcher = connection.play(stream);
+
+                dispatcher.on('end', () => voiceChannel.leave());
+            });
+            break;
+        case "skip":
+            break;
+        case "stop":
+            voiceChannel.leave();
+            break;
+    }
+
 });
+
+
+process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 
 client.login(config.token);
